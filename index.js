da957898173017e90fe92a60fac1dbaad189ce27
Logan Hart <2loganhart@gmail.com>
2018-08-13 16:31:49 -0700

    Require nodejs cluster module, simple implementation of cluster.fork() to create child servers

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration) {
-  const start = Date.now();
-  while(Date.now() - start < duration) {}
+// is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Causes index.js to be executed *again* but in child mode...
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+} else {
+  // This is the child, and will act as a server...
+  const express = require('express');
+  const app = express();
+
+  function doWork(duration) {
+    const start = Date.now();
+    while(Date.now() - start < duration) {}
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Work intensive route, takes 5 seconds');
+  });
+
+  app.get('/fast', (req, res) => {
+    res.send('Faster route, not much work required')
+  })
+
+  app.listen(3000, () => {
+    console.log('server listening on port 3000');
+  });
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send('Hi there');
-});
-
-app.listen(3000, () => {
-  console.log('server listening on port 3000');
-});
